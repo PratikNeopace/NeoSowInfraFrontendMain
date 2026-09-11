666da379296aa27b52317c2a4d6efcfc681bd5f6
PratikNeopace <pratik.g@neopaceinfotech.com>
2026-09-11 14:17:32 +0530

    fix: missing import for ForgotPassword in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import PrivateRoute from './components/PrivateRoute';
 import Login from './pages/Login';
+import ForgotPassword from './pages/ForgotPassword';
 import Home from './pages/Home';
 import QuotationList from './pages/QuotationList';
 import CustomerDetails from './pages/CustomerDetails';
